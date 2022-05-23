cbabfc75358c89fe11336f054fceacbb8cf55c1d
Ekaterina Stom <ekaterinastom@gmail.com>
2022-05-23 23:38:30 +0300

    feat: solution task 05

---
+++ 05-merge-styles/index.js
@@ -0,0 +1,16 @@
+const fs = require('fs');
+const path = require('path');
+
+fs.readdir(path.join(__dirname, 'styles'), { withFileTypes: true }, (err, files) => {
+    
+    if (err) throw err;
+    files.forEach( (file) => {
+        if (file.isFile() && path.extname(file.name) === '.css') {
+            fs.readFile (path.join(__dirname, 'styles', file.name), (err, data) => {
+                if (err) throw err; 
+                let newFile = fs.createWriteStream(path.join(__dirname, 'project-dist', 'bundle.css'));
+                newFile.write(`${data}`);
+            });
+        }    
+    });    
+});
